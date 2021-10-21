8872ec44c0e5037c67a9f1cc3783c0d63686215d
Xailaaa <Xaila.brooks.student@madisonpsb.org>
2021-10-21 15:26:38 +0000

    platformer

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -39,6 +39,9 @@
         // example:
         createPlatform(400, 250);
         createPlatform(170, 460, 0.5);
+        createPlatform(230,100,345);
+        createPlatform(460, 21, 30);
+        createPlatform(0.2, 500);
         
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
